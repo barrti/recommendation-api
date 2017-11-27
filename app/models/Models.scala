20ba96e50fd8ae3c3a06d1df4f3019549babc418
fcygan <fcygan@pgs-soft.com>
2017-11-27 15:56:48 +0100

    review

--- app/models/Models.scala
+++ app/models/Models.scala
@@ -2,4 +2,5 @@ package models
 
 case class Movie(id: Long, title: String)
 
-case class Review()
+case class Review(id: Option[Long], comment: Option[String], rate: Double, userId: Long, movieId: Long)
+
